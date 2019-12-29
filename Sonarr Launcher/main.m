0ac0701ad02afcdd78b9ae0556f6793a190d94b1
Liam Williams <l.s.w5559@gmail.com>
2019-12-29 19:29:01 +0000

    Whoops...

--- Sonarr Launcher/main.m
+++ Sonarr Launcher/main.m
@@ -8,7 +8,7 @@ int const MONO_VERSION_MINOR = 0;
 
 int main() {
     @autoreleasepool {
-        NSString *assemblyPath = [[NSBundle bundleWithPath:@"/Applications/Sonarr.app"] pathForAuxiliaryExecutable:@"Sonarr.exe"];
+        NSString *assemblyPath = [[NSBundle mainBundle] pathForAuxiliaryExecutable:@"Sonarr.exe"];
         NSLog(@"%@", assemblyPath);
         
         int exitCode = [RunWithMono runAssemblyPathWithMono:APP_NAME assemblyPath:assemblyPath major:MONO_VERSION_MAJOR minor:MONO_VERSION_MINOR];
